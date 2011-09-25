64552f4338ddf4ac859174928c59fb3b85dad353
James Gill <jamesagill@gmail.com>
2011-09-25 09:05:37 -0700

    Pruning unused ancestor code.

--- persistence.py
+++ persistence.py
@@ -2,14 +2,6 @@ from model import Doctor, Room, Patient, Task
 from google.appengine.ext import db
 
 
-def patient_key(patient_name=None):
-  """Constructs a datastore key for a Patient entity with patient_name."""
-  return db.Key.from_path('Patient', patient_name or 'default_patient')
-
-def task_key(task_name=None):
-  """Constructs a datastore key for a Task entity with task_name."""
-  return db.Key.from_path('Task', task_name or 'default_task')
-
 
 def complete_doctor_tasks(doctor):
     tasks = []
@@ -38,8 +30,6 @@ def complete_patient_tasks(patient):
 def get_tasks_for_patient(patient):
     tasks = Task.gql("WHERE patient = :patient ",
             ancestor = task_key(), patient = patient)
-    #tasks = Task.gql("WHERE ANCESTOR IS :ancestor AND patient = :patient ",
-            #ancestor = task_key(), patient = patient)
     return tasks
 
 def get_all_patients():
@@ -51,9 +41,6 @@ def get_all_patients():
 def get_patients_for_doctor(doctor):
     patients = Patient.gql("WHERE doctor = :doctor ",
             ancestor = patient_key(), doctor = doctor)
-    #patients = Patient.gql("WHERE ANCESTOR IS :ancestor AND doctor = :doctor ",
-            #ancestor = patient_key(), doctor = doctor)
-            
     for patient in patients:
         complete_patient_tasks(patient)
     return patients
