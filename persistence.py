bafebe685c838ad19c7d90865046db57c748c3d0
James Gill <jamesagill@gmail.com>
2011-09-25 08:56:58 -0700

    Patients and Doctors now have

--- persistence.py
+++ persistence.py
@@ -10,6 +10,31 @@ def task_key(task_name=None):
   """Constructs a datastore key for a Task entity with task_name."""
   return db.Key.from_path('Task', task_name or 'default_task')
 
+
+def complete_doctor_tasks(doctor):
+    tasks = []
+    for patient in doctor.patient_set():
+        tasks.append(patient.task_set)
+    for task in tasks:
+        if task.when_completed:
+            doctor.completed_tasks.put(task)
+        else:
+            doctor.pending_tasks.put(task)
+            
+def get_doctor(doctor_name):
+    doctor = Doctor.gql("WHERE name = :name ", name = doctor_name)[0]
+    complete_doctor_tasks(doctor)
+    return doctor
+            
+def complete_patient_tasks(patient):
+    tasks = []
+    for task in tasks:
+        if task.when_completed:
+            patient.completed_tasks.put(task)
+        else:
+            patient.pending_tasks.put(task)
+    
+    
 def get_tasks_for_patient(patient):
     tasks = Task.gql("WHERE patient = :patient ",
             ancestor = task_key(), patient = patient)
@@ -19,6 +44,8 @@ def get_tasks_for_patient(patient):
 
 def get_all_patients():
     patients = Patient.all()
+    for patient in patients:
+        complete_patient_tasks(patient)
     return patients
     
 def get_patients_for_doctor(doctor):
@@ -27,7 +54,15 @@ def get_patients_for_doctor(doctor):
     #patients = Patient.gql("WHERE ANCESTOR IS :ancestor AND doctor = :doctor ",
             #ancestor = patient_key(), doctor = doctor)
             
+    for patient in patients:
+        complete_patient_tasks(patient)
     return patients
+    
+def handover_patients(from_doctor, to_doctor):
+    patients = get_patients_for_doctor(from_doctor)
+    for patient in patients:
+        patient.doctor = to_doctor
+        patient.put()
     
 
     
